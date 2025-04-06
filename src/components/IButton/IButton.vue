1781ea66cf1656ef68808825e11a9802a7428399
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-04-06 23:08:11 +0300

    Add default slot

--- src/components/IButton/IButton.vue
+++ src/components/IButton/IButton.vue
@@ -1,3 +1,6 @@
 <template>
-  <button class="bg-[#FFA279] rounded-xl py-3 px-10 text-white font-bold -tracking-wider">Почати роботу</button>
+  <button class="bg-[#FFA279] rounded-xl py-3 px-10 text-white font-bold -tracking-wider">
+    <div v-if="$slots.default">Hello button slot</div>
+    <slot>Hello button</slot>
+  </button>
 </template>
