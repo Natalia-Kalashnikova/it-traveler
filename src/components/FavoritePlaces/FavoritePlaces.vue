175bc31a37c02e429c3f8ac42dfb11b9f1391573
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-04-12 02:03:11 +0300

    Add focus example

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -2,6 +2,7 @@
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue';
 import IButton from '../IButton/IButton.vue';
 import { ref, reactive } from 'vue';
+import IInput from '../IInput/IInput.vue';
 
 const buttonVariant = ref('gradient');
 
@@ -11,8 +12,12 @@ const changeButtonVariant = () => {
 </script>
 
 <template>
-      <div class="px-6">
+      <div class="px-6" @click="()=> console.log('clicked')">
         <div class="text-gray mb-4">Додані маркери</div>
+        <div class="py-5">
+          <IInput label="Some label" />
+          <a href="/" class="text-black" @click.prevent="()=> console.log('prevented')">Click me</a>
+        </div>
         <slot name="label"></slot>
         <slot name="list">
         <FavoritePlace v-for="n in 4" :key="n" />
