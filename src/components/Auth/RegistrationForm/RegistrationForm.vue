0e00d8e92103d109fadd152f30ee23ff9e825f52
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-04-14 23:53:03 +0300

    Add registration form

---
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -0,0 +1,12 @@
+<script setup>
+import IInput from '@/components/IInput/IInput.vue';
+
+</script>
+
+<template>
+  <form class="max-w-[500px] w-full bg-white p-10 rounded-2xl">
+    <IInput class="mb-4" label="Повне ім’я" />
+    <IInput class="mb-4" label="Електронна пошта" />
+    <IInput class="mb-4" label="Пароль" />
+  </form>
+</template>
