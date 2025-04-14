b10452ef17626bd339651e6ebe578e29f5c13949
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-04-15 00:18:10 +0300

    All attributes only to input as a child component

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -7,6 +7,6 @@ import IInput from '@/components/IInput/IInput.vue';
   <form class="max-w-[500px] w-full bg-white p-10 rounded-2xl">
     <IInput class="mb-4" label="Повне ім’я" />
     <IInput class="mb-4" label="Електронна пошта" placeholder="email@gmail.com" />
-    <IInput class="mb-4" label="Пароль" type="password" />
+    <IInput class="mb-4" label="Пароль" type="password" disabled />
   </form>
 </template>
