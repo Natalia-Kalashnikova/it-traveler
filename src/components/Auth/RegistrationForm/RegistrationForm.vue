2b41371156dfde17043fbf33c7bdc49ca9686f4f
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-04-23 00:35:15 +0300

    Add text area

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -15,7 +15,7 @@ const userData = reactive({
 <template>
   <FormContainer @submit.prevent="emit('submit', toRaw(userData))" class="max-w-[500px] w-full bg-white p-10 rounded-2xl">
     <IInput class="mb-4" label="Повне ім’я" v-model="userData.name" />
-    <IInput class="mb-4" label="Електронна пошта" placeholder="email@gmail.com" v-model="userData.email" />
+    <IInput type="textarea" class="mb-4" label="Електронна пошта" placeholder="email@gmail.com" v-model="userData.email" />
     <IInput class="mb-4" label="Пароль" type="password" v-model="userData.password"  />
     <IButton class="mt-10 w-full" variant="gradient" type="submit">Створити аккаунт</IButton>
   </FormContainer>
