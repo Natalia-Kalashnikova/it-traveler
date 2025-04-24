4609a8ac5756b146577bbd39745aaaf0b66adb50
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-04-25 00:50:54 +0300

    Add create place modal

---
+++ src/components/CreateNewPlaceModal/CreateNewPlaceModal.vue
@@ -0,0 +1,27 @@
+<script setup>
+import IButton from '../IButton/IButton.vue';
+import IInput from '../IInput/IInput.vue';
+import IModal from '../IModal/IModal.vue';
+import InputImage from '../InputImage/InputImage.vue';
+
+const props = defineProps({
+  isOpen: {
+    default: false,
+    type: Boolean
+  }
+})
+
+const emit = defineEmits(['close'])
+</script>
+
+<template>
+  <IModal v-if="props.isOpen" @close="emit('close')">
+    <div class="min-w-[420px]">
+      <div class="font-bold text-center">Додати маркер</div>
+      <IInput label="Локація" class="mb-4" />
+      <IInput label="Опис" type="textarea" class="mb-2" />
+      <InputImage class="mb-10" />
+      <IButton class="w-full">Додати</IButton>
+    </div>
+  </IModal>
+</template>
