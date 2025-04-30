3a35238f787e8cc58bfc9a5d6be01e75cff7fbd2
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-04-30 21:09:28 +0300

    Marker interaction

--- src/components/FavoritePlace/FavoritePlace.vue
+++ src/components/FavoritePlace/FavoritePlace.vue
@@ -12,7 +12,11 @@ const props = defineProps({
     required: true,
     type: String
   },
-  img: String
+  img: String,
+  isActive: {
+    required: true,
+    type: Boolean
+  }
 })
 </script>
 
@@ -22,17 +26,22 @@ const props = defineProps({
       <img class="w-[76px] h-[76px] shrink-0" :src="props.img" alt="" />
       <div class="w-full">
         <div class="flex justify-between items-center mb-2">
-          <h2 class="font-bold text-sm text-[#2C2C2C]">{{props.title}}</h2>
+          <h2 class="font-bold text-sm text-[#2C2C2C]">{{ props.title }}</h2>
           <div class="flex gap-2">
-            <FavoritePlaceIconButton><EditIcon/></FavoritePlaceIconButton>
-            <FavoritePlaceIconButton><DeleteIcon/></FavoritePlaceIconButton>
+            <FavoritePlaceIconButton>
+              <EditIcon />
+            </FavoritePlaceIconButton>
+            <FavoritePlaceIconButton>
+              <DeleteIcon />
+            </FavoritePlaceIconButton>
           </div>
         </div>
         <p class="text-xs line-clamp-3">
-          {{props.description}}
+          {{ props.description }}
         </p>
       </div>
     </div>
-    <div class="h-[1px] w-full bg-[#ececec] mt-4"></div>
+    <div class="h-[1px] w-full bg-[#ececec] mt-4"
+      :class="{ 'bg-primary': props.isActive, 'bg-[#ececec]': !props.isActive }"></div>
   </section>
 </template>
