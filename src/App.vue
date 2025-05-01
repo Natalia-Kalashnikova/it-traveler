b7d7303ee25bff4af9d7eefa33621228669eb93e
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-05-01 20:02:29 +0300

    Router setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup>
-import AuthView from './views/AuthView.vue';
-import GreetingView from './views/GreetingView.vue';
-import HomepageView from './views/HomepageView.vue';
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
- <!-- <GreetingView />
- <HomepageView /> -->
- <AuthView />
+ <router-view />
 </template>
