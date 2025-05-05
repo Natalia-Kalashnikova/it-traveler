d3cd3ab0dec1ac9c8bc9f9af54179b8d66ea86c1
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-05-05 22:26:36 +0300

    Add nadd nested routesested routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,23 @@ import { createRouter, createWebHistory } from 'vue-router'
 const GreetingPage = () => import('../views/GreetingView.vue')
 const Homepage = () => import('../views/HomepageView.vue')
 const AuthPage = () => import('../views/AuthView.vue')
+const LoginPage = () => import('../views/LoginView.vue')
+const RegistrationPage = () => import('../views/RegistrationView.vue')
 
- const routes = [
-   { path: '/', component: GreetingPage },
-   { path: '/map', component: Homepage },
-   { path: '/auth', component: AuthPage }
- ]
+const routes = [
+  { path: '/', component: GreetingPage },
+  { path: '/map', component: Homepage },
+  {
+    path: '/auth',
+    component: AuthPage,
+    children: [
+      { path: 'login', component: LoginPage },
+      { path: 'registration', component: RegistrationPage }
+    ]
+  }
+]
 
- export const router = createRouter({
-   history: createWebHistory(),
-   routes
- })
+export const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,8 +1,10 @@
 <script setup>
 import BaseLayout from '@/layouts/BaseLayout.vue';
+import { RouterView } from 'vue-router';
 
 </script>
 <template>
   <BaseLayout>
+    <router-view></router-view>
   </BaseLayout>
 </template>
