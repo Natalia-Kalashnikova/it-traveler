ffe977f0d0ae448206a5a10e64a87fea015fea35
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-05-06 00:39:46 +0300

    Add additional navigation

--- src/components/IButton/IButton.vue
+++ src/components/IButton/IButton.vue
@@ -1,5 +1,7 @@
 <script setup>
-import { defineProps, computed } from 'vue'
+import { defineProps, computed } from 'vue';
+import { RouterLink } from 'vue-router';
+
 
 const props = defineProps({
   variant: {
@@ -8,17 +10,35 @@ const props = defineProps({
     validator: (value) => {
       return ['primary', 'gradient', 'outlined'].includes(value)
     }
-  }
+  },
+  to: String
 })
 
 const bgStyles = computed(() => {
-  return props.variant === 'gradient' ? 'bg-gradient-to-r from-[#FFA279] to-[#F3743D]' : 'bg-[#FFA279]'
+  return props.variant === 'gradient'
+    ? 'bg-gradient-to-r from-[#FFA279] to-[#F3743D]'
+    : 'bg-[#FFA279]'
+})
+
+const isLink = computed(() => !!props.to)
+
+const componentName = computed(() => {
+   return isLink.value ? RouterLink : 'button'
+ })
+
+const link = computed(() => {
+  return  isLink.value ? props.to : undefined
 })
 </script>
 
 
 <template>
-  <button class="rounded-xl py-3 px-10 text-white font-bold -tracking-wider" :class='bgStyles'>
+  <component
+    :is="componentName"
+    class="rounded-xl py-3 px-10 text-white font-bold -tracking-wider"
+    :class='bgStyles'
+    :to="link"
+  >
       <slot></slot>
-  </button>
+  </component>
 </template>
--- src/views/GreetingView.vue
+++ src/views/GreetingView.vue
@@ -9,7 +9,7 @@ import IButton from '../components/IButton/IButton.vue'
       <img class="inline mb-6" src="../assets/img/map-pin.svg" alt="map-pin" />
       <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
       <p class="leading-6 mb-11">Простий і зручний веб додаток, який дозволить тобі відмічати твої улюблені місця, а також ті, в яких би ти дуже хотів побувати. Тож не зволікай і спробуй сам.</p>
-      <IButton>Почати роботу</IButton>
+      <IButton to="/auth/registration">Почати роботу</IButton>
     </div>
   </BaseLayout>
 </template>
