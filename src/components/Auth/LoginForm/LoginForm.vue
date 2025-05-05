76ff32cd4d9fb31fd8c887bf0931f3b9615c7b11
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-05-06 00:19:43 +0300

    Add nested navigation

--- src/components/Auth/LoginForm/LoginForm.vue
+++ src/components/Auth/LoginForm/LoginForm.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { reactive, watch } from 'vue';
-import FormContainer from '../FormContainer.vue';
 import IInput from '@/components/IInput/IInput.vue';
 import IButton from '@/components/IButton/IButton.vue';
 
@@ -20,9 +19,9 @@ watch(
 </script>
 
 <template>
-  <FormContainer @submit.prevent="emit('submit', userData)">
+  <form @submit.prevent="emit('submit', userData)">
     <IInput class="mb-4" label="Електронна пошта" placeholder="email@gmail.com" v-model="userData.email" />
     <IInput class="mb-4" label="Пароль" type="password" v-model="userData.password" />
     <IButton class="mt-10 w-full" variant="gradient" type="submit">Увійти</IButton>
-  </FormContainer>
+  </form>
 </template>
--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -2,7 +2,6 @@
 import IButton from '@/components/IButton/IButton.vue';
 import IInput from '@/components/IInput/IInput.vue';
 import { reactive, toRaw } from 'vue';
-import FormContainer from '../FormContainer.vue';
 
 const emit = defineEmits(['submit']);
 const userData = reactive({
@@ -13,10 +12,10 @@ const userData = reactive({
 </script>
 
 <template>
-  <FormContainer @submit.prevent="emit('submit', toRaw(userData))" class="max-w-[500px] w-full bg-white p-10 rounded-2xl">
+  <form @submit.prevent="emit('submit', toRaw(userData))" >
     <IInput class="mb-4" label="Повне ім’я" v-model="userData.name" />
     <IInput type="textarea" class="mb-4" label="Електронна пошта" placeholder="email@gmail.com" v-model="userData.email" />
     <IInput class="mb-4" label="Пароль" type="password" v-model="userData.password"  />
     <IButton class="mt-10 w-full" variant="gradient" type="submit">Створити аккаунт</IButton>
-  </FormContainer>
+  </form>
 </template>
