2f466f4499b6ee2f5dbc7ae9e83819634709fa66
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-05-13 23:56:28 +0300

    Add login form

--- src/components/Auth/LoginForm/LoginForm.vue
+++ src/components/Auth/LoginForm/LoginForm.vue
@@ -3,6 +3,12 @@ import { reactive, watch } from 'vue';
 import IInput from '@/components/IInput/IInput.vue';
 import IButton from '@/components/IButton/IButton.vue';
 
+const props = defineProps({
+  isLoading: {
+    default: false,
+    type: Boolean
+  }
+})
 const emit = defineEmits(['submit']);
 const userData = reactive({
   email: '',
@@ -22,6 +28,8 @@ watch(
   <form @submit.prevent="emit('submit', userData)">
     <IInput class="mb-4" label="Електронна пошта" placeholder="email@gmail.com" v-model="userData.email" />
     <IInput class="mb-4" label="Пароль" type="password" v-model="userData.password" />
-    <IButton class="mt-10 w-full" variant="gradient" type="submit">Увійти</IButton>
+    <IButton class="mt-10 w-full" variant="gradient" type="submit" :is-loading="props.isLoading">
+      Увійти
+    </IButton>
   </form>
 </template>
