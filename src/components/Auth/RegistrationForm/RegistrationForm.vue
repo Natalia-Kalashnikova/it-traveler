337d3e0cef4cda22db02519417a56cbbf2bd7d64
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-05-14 00:36:04 +0300

    Add composable

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -21,7 +21,7 @@ const userData = reactive({
 <template>
   <form @submit.prevent="emit('submit', toRaw(userData))" >
     <IInput class="mb-4" label="Повне ім’я" v-model="userData.name" />
-    <IInput type="textarea" class="mb-4" label="Електронна пошта" placeholder="email@gmail.com" v-model="userData.email" />
+    <IInput  class="mb-4" label="Електронна пошта" placeholder="email@gmail.com" v-model="userData.email" />
     <IInput class="mb-4" label="Пароль" type="password" v-model="userData.password"  />
     <IButton class="mt-10 w-full" variant="gradient" type="submit" :is-loading="props.isLoading">
       Створити аккаунт
